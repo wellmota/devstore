1bec36a8c243fa3aa0c0db990654ca74ce72586b
wellmota <impeto.wellington@gmail.com>
2025-01-29 12:52:40 -0300

    feat: add lucide-react dependency and enhance header component with search and cart functionality

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -3,8 +3,8 @@ import { ReactNode } from 'react'
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
-    <div>
-     <Header/>
+    <div className='mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-[min-content_max-content] gap-5 p-8'>
+      <Header />
       {children}
     </div>
   )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,33 @@
-import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: 'devstore',
-}
+  title: "devstore",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-zinc-950 text-zinc-50 antialiased`}
       >
         {children}
       </body>
     </html>
-  )
+  );
 }

33da525169a9b60f5ce73d2b2432c6433b0f3ab4
wellmota <impeto.wellington@gmail.com>
2025-01-29 19:06:34 -0300

    feat: implement featured products display and update product image paths

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,15 +1,26 @@
+import { api } from "@/data/api";
+import { Product } from "@/data/types/product";
 import Image from "next/image";
 import Link from "next/link";
 
+async function getFeaturedProducts(): Promise<Product[]> {
+  const response = await api("/products/featured");
+  const products = await response.json();
+  return products;
+}
+
 export default async function Home() {
+  const [highlightedProduct, ...otherProducts] = await getFeaturedProducts();
+  console.log(highlightedProduct);
+
   return (
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
-        href="/"
+        href={`/products/${highlightedProduct.slug}`}
         className="group relative col-span-6 row-span-6 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
       >
         <Image
-          src="/images/moletom-ai-side.png"
+          src={highlightedProduct.image}
           alt="moletom ai side"
           width={920}
           height={920}
@@ -17,50 +28,47 @@ export default async function Home() {
           className="transition-transform duration-300 group-hover:scale-105"
         />
         <div className="absolute bottom-28 right-28 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/80 p-1 pl-5">
-          <span className="truncate text-sm">AI hoodie side</span>
+          <span className="truncate text-sm">{highlightedProduct.title}</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            $129
+            {highlightedProduct.price.toLocaleString("en-US", {
+              style: "currency",
+              currency: "USD",
+              minimumFractionDigits: 0,
+              maximumFractionDigits: 0,
+            })}
           </span>
         </div>
       </Link>
-      <Link
-        href="/"
-        className="group relative col-span-3 row-span-3 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
-      >
-        <Image
-          src="/images/moletom-java.png"
-          alt="moletom ai side"
-          width={920}
-          height={920}
-          quality={100}
-          className="transition-transform duration-300 group-hover:scale-105"
-        />
-        <div className="absolute bottom-10 right-28 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/80 p-1 pl-5">
-          <span className="truncate text-sm">AI hoodie side</span>
-          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            $129
-          </span>
-        </div>
-      </Link>
-      <Link
-        href="/"
-        className="group relative col-span-3 row-span-3 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
-      >
-        <Image
-          src="/images/moletom-never-stop-learning.png"
-          alt="moletom ai side"
-          width={920}
-          height={920}
-          quality={100}
-          className="transition-transform duration-300 group-hover:scale-105"
-        />
-        <div className="absolute bottom-10 right-28 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/80 p-1 pl-5">
-          <span className="truncate text-sm">AI hoodie side</span>
-          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            $129
-          </span>
-        </div>
-      </Link>
+
+      {otherProducts.map((product) => {
+        return (
+          <Link
+            key={product.slug}
+            href="/"
+            className="group relative col-span-3 row-span-3 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
+          >
+            <Image
+              src={product.image}
+              alt="moletom ai side"
+              width={920}
+              height={920}
+              quality={100}
+              className="transition-transform duration-300 group-hover:scale-105"
+            />
+            <div className="absolute bottom-10 right-28 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/80 p-1 pl-5">
+              <span className="truncate text-sm">{product.title}</span>
+              <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+                {product.price.toLocaleString("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                  minimumFractionDigits: 0,
+                  maximumFractionDigits: 0,
+                })}
+              </span>
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 }
--- src/env.ts
+++ src/env.ts
@@ -1,22 +1,17 @@
 import { z } from "zod";
 
-// Define the schema for environment variables
 const envSchema = z.object({
-  NEXT_PUBLIC_API_BASE_URL: z.string().url(), // The API base URL must be a valid URL string
+  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
 });
 
-// Parse and validate the environment variables against the schema
 const parsedEnv = envSchema.safeParse(process.env);
 
-// Check if the parsing and validation were successful
 if (!parsedEnv.success) {
-  // If validation fails, log an error message with the details
-  console.error(
-    "Invalid environment variables",
-    parsedEnv.error.flatten().fieldErrors,
-  );
-
-  throw new Error("Invalid environment variables");
+  console.error("Invalid env variables", parsedEnv.error.flatten().fieldErrors);
+  throw new Error("Invalid env variables");
 }
+
+
+
 
 export const env = parsedEnv.data;

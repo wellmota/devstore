6acaaf9981bd3b0829ed951235de178d233afa74
wellmota <impeto.wellington@gmail.com>
2025-01-30 13:07:29 -0300

    feat: implement product details fetching and display on product page

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,12 +1,34 @@
-import { ShoppingCart } from 'lucide-react'
+import { api } from "@/data/api";
+import { Product } from "@/data/types/product";
+import { ShoppingCart } from "lucide-react";
 import Image from "next/image";
 
-export default function ProductPage() {
+interface ProductProps {
+  params: {
+    slug: string;
+  };
+}
+
+async function getProduct(slug: string): Promise<Product> {
+  const response = await api(`/products/${slug}`, {
+    next: {
+      revalidate: 60 * 60, // 1 hour
+    },
+  });
+
+  const products = await response.json();
+
+  return products;
+}
+
+export default async function ProductPage({ params }: ProductProps) {
+  const product = await getProduct(params.slug);
+
   return (
     <div className="relative grid max-h-[860px] grid-cols-3">
       <div className="col-span-2 overflow-hidden">
         <Image
-          src="/images/moletom-never-stop-learning.png"
+          src={product.image}
           alt="Hoodie within phrase Never Stop Learning"
           width={1000}
           height={1000}
@@ -15,18 +37,25 @@ export default function ProductPage() {
       </div>
 
       <div className="flex flex-col justify-center px-12">
-        <h1 className="learning-tight text-3xl font-bold">
-          Never Stop Learning Hoodie
-        </h1>
+        <h1 className="learning-tight text-3xl font-bold">{product.title}</h1>
         <p className="mt-2 leading-relaxed text-zinc-400">
-          Fabric Hoodie 88% Cotton, 12% Polyester. Machine wash cold.
+          {product.description}
         </p>
         <div className="mt-8 flex items-center gap-3">
           <span className="inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
-            $129
+            {product.price.toLocaleString("en-US", {
+              style: "currency",
+              currency: "USD",
+              minimumFractionDigits: 0,
+              maximumFractionDigits: 0,
+            })}
           </span>
           <span className="text-sm text-zinc-400">
-            12x installments of $10.75
+            12x installments of{" "}
+            {(product.price / 12).toLocaleString("en-US", {
+              style: "currency",
+              currency: "USD",
+            })}
           </span>
         </div>
 
@@ -69,7 +98,7 @@ export default function ProductPage() {
           type="button"
           className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold uppercase text-white"
         >
-          <ShoppingCart className="w-6 h-6 mr-2" />
+          <ShoppingCart className="mr-2 h-6 w-6" />
           Add to cart
         </button>
       </div>

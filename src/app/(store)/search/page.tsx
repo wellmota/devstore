9d3ba0a7d9ce5c91ca447a2dae9fe6249d5ff813
wellmota <impeto.wellington@gmail.com>
2025-01-31 09:22:07 -0300

    feat: enhance search page with product display and details

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -1,4 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default async function Search() {
-  // await new Promise((resolve) => setTimeout(resolve, 1000))
-  return <h1>Search</h1>
+  return (
+    <div className="flex flex-col gap-4">
+      <p>
+        Resultados para: <span className="font-semibold">moletom</span>
+      </p>
+
+      <div className="grid grid-cols-3 gap-6">
+        <Link
+          href="/product/moletom-never-stop-learning"
+          className="group relative flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
+        >
+          <Image
+            src="/images/moletom-never-stop-learning.png"
+            alt="moletom ai side"
+            width={480}
+            height={480}
+            quality={100}
+            className="transition-transform duration-300 group-hover:scale-105"
+          />
+          <div className="absolute bottom-10 right-28 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/80 p-1 pl-5">
+            <span className="truncate text-sm">Never stop learning Hoodie</span>
+            <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+              {Number(99.99).toLocaleString("en-US", {
+                style: "currency",
+                currency: "USD",
+                minimumFractionDigits: 0,
+                maximumFractionDigits: 0,
+              })}
+            </span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }

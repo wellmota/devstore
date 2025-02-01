620b55d06b714ebb9ee951e9f41bca3b12075f51
wellmota <impeto.wellington@gmail.com>
2025-02-01 10:10:20 -0300

    feat: enhance search loading component with dynamic query display and skeleton loading indicators

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,3 +1,25 @@
+"use client";
+
+import { Skeleton } from "@/components/skeleton";
+import { useSearchParams } from "next/navigation";
+
 export default function SearchLoading() {
-  return <h1>Loading search...</h1>
+  const searchParams = useSearchParams();
+  const query = searchParams.get("q");
+
+  return (
+    <div className="flex flex-col gap-4">
+      <p>
+        Result for:<span className="text-sm">{query}</span>
+      </p>
+      <div className="grid-col-3 grid gap-6">
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+      </div>
+    </div>
+  );
 }

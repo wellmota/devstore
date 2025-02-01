cfed10b9595b266aa3afea9be194d6d3024c2591
wellmota <impeto.wellington@gmail.com>
2025-02-01 09:30:40 -0300

    feat: create a reusable search form component and integrate it into the header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
-import { Search } from "lucide-react";
+
 import Image from "next/image";
-import CardWidget from './card-widget'
+import CardWidget from "./card-widget";
+import { SearchForm } from "./search-form";
 
 export function Header() {
   return (
@@ -11,17 +12,10 @@ export function Header() {
           <Link href="/" className="text-2xl font-extrabold text-white">
             devstore
           </Link>
-          <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
-            <Search className="h-5 w-5 text-zinc-500" />
-            <input
-              placeholder="Search products..."
-              type="text"
-              className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
-            />
-          </form>
+          <SearchForm />
         </div>
         <div className="flex items-center gap-4">
-          <CardWidget/>
+          <CardWidget />
           <div className="h-4 w-px bg-zinc-700" />
           <Link href="/" className="flex items-center gap-2 hover:underline">
             <span className="text-sm">Account</span>
---
+++ src/components/search-form.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { Search } from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { FormEvent } from "react";
+
+export function SearchForm() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("q");
+
+  function handleSearch(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const data = Object.fromEntries(formData) as { q?: string };
+
+    const query = data.q;
+    console.log(query);
+
+    if (!query) {
+      return null;
+    }
+
+    router.push(`/search?q=${query}`);
+  }
+
+  return (
+    <form
+      onSubmit={handleSearch}
+      className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700"
+    >
+      <Search className="h-5 w-5 text-zinc-500" />
+      <input
+        name="q"
+        defaultValue={query ?? ""}
+        placeholder="Search products..."
+        type="text"
+        className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
+        required
+      />
+    </form>
+  );
+}
